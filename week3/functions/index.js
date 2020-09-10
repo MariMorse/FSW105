0a3c6dd53df1d9c10c8c76428d4df90304867acc
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-10 11:36:27 -0400

    commit

---
+++ week3/functions/index.js
@@ -0,0 +1,30 @@
+console.log("\n***Question 1***")
+function sum(num1, num2){
+        return num1 + num2
+}
+var result = sum(20, 13)
+console.log(result)
+
+console.log("\n***Question 2***")
+function largestOfThree(para1, para2, para3) {
+    return Math.max.apply(Math, array);
+}
+
+let array = [36, 78, 5];
+console.log(largestOfThree(array))
+
+console.log('\n***Question 3***')
+
+function oddOrEven(n){
+    if(n % 2 == 0) {
+        return "even";
+    } else if (n % 2 != 0) {
+        return "odd";
+    }       
+}
+var answer = oddOrEven(35);
+    console.log(answer);
+
+
+    
+
