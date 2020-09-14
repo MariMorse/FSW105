c175c780fdbbe9b1a51f0b7d9fdbb902f37c9294
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-13 21:39:51 -0400

    commit

--- week4/string&array_methods/index.js
+++ week4/string&array_methods/index.js
@@ -8,8 +8,8 @@ console.log(capitalizeAndLowercase("Hello"))
 
 console.log("\n***Question 2***")
 function findMiddleIndex(str) {
-    var middleOfString = Math.floor(str.length / 2);
-    return "Half of the String's length rounded down are " + middleOfString;
+    total_length = len(str)
+    second_half_first_index = Math.floor(total_length / 2)
 }
 console.log(findMiddleIndex("Hello"));
 console.log(findMiddleIndex("Hello World"));
@@ -24,8 +24,8 @@ console.log(returnFirstHalf("Hello World"));
 
 console.log("\n***Question 4***")
 function upperAndLower(str) {
-    var hUpperHLower = str.toUpperCase()
-    return hUpperHLower
+    var strLength = (str.length);
+    
 }
 console.log(upperAndLower("Hello"));
 console.log(upperAndLower("Hello World"));
