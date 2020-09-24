93194f54462105ac3ac1d04314f21e6a52490e6a
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-24 18:51:47 -0400

    commit

---
+++ week5/es6/es6.js
@@ -0,0 +1,32 @@
+console.log("\n***Question 1***")
+//let name = "John"
+//let age = 101
+//
+//function runForLoop(pets) {
+//    let petObjects = []
+//    for(let i = 0; i < pets.length; i++) {
+//        name;
+//        if(pets[i] === "cat") {
+//            name = "fluffy"
+//        } else {
+//            name = "spot"
+//        }
+//        console.log("pet name: ", name)
+//        pets.name = namePetObjects.push(pet)
+//    }
+//    console.log("man name: ", name)
+//   return namePetObjects 
+//}
+//runForLoop(["cat", "dog"]);
+
+console.log("\n***Question 2***");
+//const carrots = ["bright orange", "ripe", "rotten"];
+//
+//mapVegetables = arr => {
+//    return arr.map(carrot => {
+//        return {type: "carrot", name: carrot}
+//});
+//}
+//console.log(mapVegetables([carrots]));
+
+console.log("\n***Question 4***");
