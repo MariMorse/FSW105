d3e0ebe33a0f62b188b956d212ac57859bbde1c2
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-24 21:57:06 -0400

    commit

--- week5/array_exercises/index.js
+++ week5/array_exercises/index.js
@@ -1,6 +1,5 @@
 //***Section A- .filter()
 console.log("***A-1***");
-{
 function fiveAndGreaterOnly(arr) {
     const result = arr.filter(function (num) {
         if (num >= 5) {
@@ -10,98 +9,88 @@ function fiveAndGreaterOnly(arr) {
     return result;
 };
 console.log(fiveAndGreaterOnly([3, 6, 8, 2]));
-}
+
 console.log("\n***A-2***");
-{
-function evensOnly(arr2) {
-    const resultOne = arr2.filter(function (numEvens) {
-        if (numEvens % 2 === 0) {
-            return true;
-        };
-    });
-    return resultOne;
-};
-console.log(evensOnly([3, 6, 8, 2]));
-}
+const evensOnly = [3, 6, 8, 2]
+
+const evens = evensOnly.filter(a => a % 2 === 0);
+console.log(evens)
+
 //Section B .map
 console.log("\n***B-1***");
-{
+
 function doubleNumbers(arrTwo){
     let double = arrTwo.map(i=>i*2);
     return double;
 }
 console.log(doubleNumbers([2, 5, 100]));
+
+console.log("\n***B-2***");
+function stringItUp(arr){
+    let string = arr.map(String);
+    return string;
 }
-//console.log("\n***B-2***");
-//function stringItUp(arr){
-//    let string = arr.map(String);
-//    return string;
-//}
-//console.log(stringItUp([2, 5, 100]));
-//console.log("\n***B-3***");
-//function capitalizeNames(arr) {
-//    let upperCase = arr.map(function(string){
-//        return string.substr(0,1).toUpperCase() + string.substr(1).toLowerCase();
-//      })
-//      return upperCase
-//}
-//console.log(capitalizeNames(["john", "JACOB", "jinGLehiemer", "schmidt"]));
+console.log(stringItUp([2, 5, 100]));
+console.log("\n***B-3***");
+function capitalizeNames(arr) {
+    let upperCase = arr.map(function(string){
+        return string.substr(0,1).toUpperCase() + string.substr(1).toLowerCase();
+})
+      return upperCase
+}
+console.log(capitalizeNames(["john", "JACOB", "jinGLehiemer", "schmidt"]));
 //Section C- .reduce()
-//console.log("\n***C-1***");
+console.log("\n***C-1***");
 //I chose to use the ES6 because it shortens the code into 3 lines, it is also easier to read, and I like the ES6.
-//const nums = [1, 2, 3]
+const nums = [1, 2, 3]
 
-//const total = nums.reduce((final, num) => final + num);  
-//console.log(total)
+const total = nums.reduce((final, num) => final + num);  
+console.log(total)
 
-//console.log("\n***C-2***");
-//const stringConcat = [1, 2, 3]
-//
-//const result = stringConcat.reduce(function(final, stringNums){
-//    
-//}
-//console.log(result)
+console.log("\n***C-2***");
+stringConcat = (arr) => arr.reduce((final, num) => final + num.toString());
 
-//console.log("\n***C-3***");
-//const totalVotes = [
-//    {name: 'Bob', age: 30, voted: true},
-//    {name: 'Jake', age: 32, voted: true},
-//    {name: 'Kate', age: 25, voted: false},
-//    {name: 'Sam', age: 20, voted: false},
-//    {name: 'Phil', age: 21, voted: true},
-//    {name: 'Ed', age: 55, voted: true},
-//    {name: 'Tami', age: 54, voted: true},
-//    {name: 'Mary', age: 31, voted: false},
-//    {name: 'Becky', age: 43, voted: false},
-//    {name: 'Joey', age: 41, voted: true},
-//    {name: 'Jeff', age: 30, voted: true},
-//    {name: 'Zack', age: 19, voted: false}
-//]
-//
-//const voteCount = totalVotes.reduce(function(final, voter){
-//    if(voter.voted) {
-//        final++
-//    }
-//    return final
-//}, 0) 
-//
-//console.log(voteCount)
+console.log(stringConcat([1,2,3]));
+
+console.log("\n***C-3***");
+const totalVotes = [
+    {name: 'Bob', age: 30, voted: true},
+    {name: 'Jake', age: 32, voted: true},
+    {name: 'Kate', age: 25, voted: false},
+    {name: 'Sam', age: 20, voted: false},
+    {name: 'Phil', age: 21, voted: true},
+    {name: 'Ed', age: 55, voted: true},
+    {name: 'Tami', age: 54, voted: true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+]
+
+const voteCount = totalVotes.reduce(function(final, voter){
+    if(voter.voted) {
+        final++
+    }
+    return final
+}, 0) 
+
+console.log(voteCount)
 //Section D .sort()
-//console.log("\n***D-1***")
-//const leastToGreatest = [1, 3, 5, 2, 90, 20]
-//    
-//leastToGreatest.sort((a, b) => a - b)
-//
-//console.log(leastToGreatest);
+console.log("\n***D-1***")
+const leastToGreatest = [1, 3, 5, 2, 90, 20]
+    
+leastToGreatest.sort((a, b) => a - b)
+console.log(leastToGreatest);
 
-//console.log("\n***D-2***");
-//const largestToSmallest = [1, 3, 5, 2, 90, 20]
-//
-//largestToSmallest.sort(function(a, b){
-//    return b - a
-//});
-//
-//console.log(largestToSmallest)
+console.log("\n***D-2***");
+const largestToSmallest = [1, 3, 5, 2, 90, 20]
+
+largestToSmallest.sort(function(a, b){
+    return b - a
+});
+
+console.log(largestToSmallest)
 
 console.log("\n***D-3***");
 const lengthSort = ["dog", "wolf", "by", "family", "eaten"]
