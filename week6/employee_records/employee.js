a1c580d1f82e9ec2b2aedbbbb8a6894559d9ea08
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-28 22:19:24 -0400

    commit

---
+++ week6/employee_records/employee.js
@@ -0,0 +1,16 @@
+function Employee(Name, Job_Title, Salary, Status = "Full Time") {
+    this.Name = Name;
+    this.Job_Title = Job_Title;
+    this.Salary = Salary;
+    this.Status = Status;
+    
+
+};
+
+var employee1 = new Employee("Clark Kent", "Reporter", "$35,860/yearly")
+var employee2 = new Employee("Bruce Banner", "Research Scientist", "$97,000/yearly")
+var employee3 = new Employee("Anna Nicole", "Escort", "$7,000/nightly", "Contract")
+
+
+
+console.log(printEmployeeForm(employee1));
