af55b3420092728c0a8c76258105484abadf9f02
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-29 11:35:40 -0400

    commit

--- week6/escape_room_simulation/escape.js
+++ week6/escape_room_simulation/escape.js
@@ -1,3 +1,13 @@
-const readline = require("readline-sync");
-const name = readline.question("What is your name? ");
+const readlineSync = require("readline-sync");
+const player = readlineSync.question("May I have your name? ");
 
+const welcomeM = `Welcome ${player} to Mari's Escape Room Simulation Game!`;
+console.log(welcomeM);
+
+let stillAlive = true;
+let foundKey = false;
+
+while(stillAlive == true)
+{
+    const optionsMenu = readlineSync.keyIn(`Hit 1...5 key: `, )
+}
