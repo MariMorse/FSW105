e14bfa6f29c4f9e1a521bd7c99f736773f097840
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-10-01 11:00:27 -0400

    commit

--- week6/escape_room_simulation/escape.js
+++ week6/escape_room_simulation/escape.js
@@ -1,14 +1,14 @@
 const readlineSync = require("readline-sync");
 const player = readlineSync.question("May I have your name? ");
 
-const welcomeM = `Welcome ${player} to Mari's Escape Room Simulation Game!`;
+const welcomeM = `Welcome ${player} to 13th Floor Escape Room Simulation Game!`;
 console.log(welcomeM);
 
 let stillAlive = true;
 let foundKey = false;
 
 while (stillAlive == true) {
-    const optionsMenu = readlineSync.keyIn('Push 1 to Put hand in the hole \n  Push 2 to Find the key \n Push 3 to Open the door', { limit: '$<1-3>' });
+    const optionsMenu = readlineSync.keyIn('\n Press 1 to Put hand in the hole \n Press 2 to Find the key \n Press 3 to Open the door', { limit: '$<1-3>' });
 
     if (optionsMenu == 1) {
         //Player is DEAD!!
@@ -20,7 +20,7 @@ while (stillAlive == true) {
         //Player needs to Find the key
         console.log(`${player} great job, you found the key!`);
         foundKey = true;
-    } else if (optionsMenu == 3 && foundKey == true) {
+    } else if (optionsMenu == 2 && foundKey == true) {
         //Player has Found the Key
         //Player and  she/he needs to go to Option 3 to Open the Door
         console.log(`${player}, you have ALREADY found the key... Please proceed to Option-3`);
@@ -33,7 +33,7 @@ while (stillAlive == true) {
         //Player has found the key and the door is open!!.
         //Game is OVER!! Great job Player!
         console.log(`Congradulations ${player}, you have successfully opened the DOOR and WON the game!!`);
-        foundKey = false;
+        stillAlive = false;
     }
 
 }
