99a9ef97320d165c588a6b488403aa33fba2a2e3
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-10-03 11:06:32 -0400

    commit

--- week6/employee_records/employee.js
+++ week6/employee_records/employee.js
@@ -1,20 +1,18 @@
 const employees = [
-    
+
 ]
-
-
-
-
 
 function Employee(Name, Job_Title, Salary, Status = "Full Time") {
     this.Name = Name;
     this.Job_Title = Job_Title;
     this.Salary = Salary;
     this.Status = Status;
-    
-
 };
 
 var employee1 = new Employee("Clark Kent", "Reporter", "$35,860/yearly")
 var employee2 = new Employee("Bruce Banner", "Research Scientist", "$97,000/yearly")
 var employee3 = new Employee("Anna Nicole", "Escort", "$7,000/nightly", "Contract")
+
+let printString = (employee1.value, employee2.value, employee3.value)
+
+console.log(printString);
