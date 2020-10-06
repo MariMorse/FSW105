a33992d97b9662a2fc522d15acaec81e5d0ed093
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-10-06 10:11:37 -0400

    commit

--- week6/employee_records/employee.js
+++ week6/employee_records/employee.js
@@ -1,18 +1,23 @@
-const employees = [
+let employees = [
 
-]
+];
 
 function Employee(Name, Job_Title, Salary, Status = "Full Time") {
     this.Name = Name;
     this.Job_Title = Job_Title;
     this.Salary = Salary;
     this.Status = Status;
-};
+    this.printEmployeeForm = () => {
+        return this.Name + " " + this.Job_Title + " " + this.Salary + " " + this.Status
+    }
+}
 
 var employee1 = new Employee("Clark Kent", "Reporter", "$35,860/yearly")
 var employee2 = new Employee("Bruce Banner", "Research Scientist", "$97,000/yearly")
 var employee3 = new Employee("Anna Nicole", "Escort", "$7,000/nightly", "Contract")
 
-let printEmployeeForm = Employee(this.Name, this.Job_Title,  this.Salary,  this.Status) 
+console.log(employee1.printEmployeeForm());
+console.log(employee2.printEmployeeForm());
+console.log(employee3.printEmployeeForm());
 
-console.log(printEmployeeForm(employee1));
+employees.push(Employee(employee1));
