bf1aab3618f505e0acc385377dad587efae3ba0a
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-10-11 15:59:50 -0400

    cpmmit

--- week7/console.rpg/rpg.js
+++ week7/console.rpg/rpg.js
@@ -1,2 +1,17 @@
 const readlineSync = require("readline-sync");
-const player = readlineSync.question("What is your name? ");
+const player = readlineSync.question("What is your name brave one? ");
+
+let welcomeMessage = `Welcome ${player} to the 
+Mushroom Kingdom, you need to reach the castle and save the princess from the Mushroom King, but beware... many enemies are waiting for you!!`
+    console.log(welcomeMessage)
+
+const enemies = ['Ninji', 'Lava Bubble', 'Fire Snake', 'Dry Bones']
+let enemyAttack = 10
+
+const specialItems = ['Fire Flower', 'Starman', 'Amulet', 'Cape']
+let itemPickup = []
+
+let playerHealth = 45
+let playerAttack = 10
+
+const options = 
